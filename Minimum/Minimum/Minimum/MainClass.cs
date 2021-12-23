a8cef3d2f69d25166f0665c5780d057872750dd7
HolmesChang <cct19840608@gmail.com>
2021-12-23 13:28:08 +0800

    Adding: Adding Project - Minimum

---
+++ Minimum/Minimum/Minimum/MainClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Minimum
+{
+    class MainClass
+    {
+        static void Main (String [] args)
+        {
+            Console.WriteLine("Hello C#");
+
+            Console.ReadKey();
+        }
+    }
+}
