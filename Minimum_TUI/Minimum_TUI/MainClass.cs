22cde83b5b548251c700179d5a5fd272cf2c6ded
HolmesChang <cct19840608@gmail.com>
2021-12-24 12:04:22 +0800

    Writing: ReArranging Structure Of Repository

---
+++ Minimum_TUI/Minimum_TUI/MainClass.cs
@@ -0,0 +1,13 @@
+﻿namespace Minimum
+{
+    class MainClass
+    {
+        static void Main (System.String [] args)
+        {
+            System.Console.WriteLine("Hello C#");
+
+            System.Console.WriteLine("Press Any Key To Exit");
+            System.Console.ReadKey();
+        }
+    }
+}
