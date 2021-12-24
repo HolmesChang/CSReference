66651387f61e1bb71ad752703de1d70c3541e247
HolmesChang <cct19840608@gmail.com>
2021-12-24 13:27:31 +0800

    Adding: Adding Project - GUI_WinFrom_Button

--- Minimum_GUI_WinForm/Minimum_GUI_WinForm/MainClass.cs
+++ Minimum_GUI_WinForm/Minimum_GUI_WinForm/MainClass.cs
@@ -1,17 +1,19 @@
-﻿public class MainClass : System.Windows.Forms.Form
+﻿namespace Minimum_GUI_WinFrom
 {
-
-    public MainClass()
+    public class MainClass : System.Windows.Forms.Form
     {
-        this.Text = "Hello GUI WinForm";
-    }
-    [System.STAThread]
-    public static void Main(string [] args)
-    {
-        MainClass MyForm = new MainClass();
-        // The Application.Run method processes messages from the operating system
-        // to your application. If you comment out the next line of code,
-        // your application will compile and execute, but because it is not in the // message loop, it will exit after an instance of the form is created.  
-        System.Windows.Forms.Application.Run(MyForm);
+        public MainClass()
+        {
+            this.Text = "Hello GUI WinForm";
+        }
+        [System.STAThread]
+        public static void Main(string[] args)
+        {
+            MainClass MyForm = new MainClass();
+            // The Application.Run method processes messages from the operating system
+            // to your application. If you comment out the next line of code,
+            // your application will compile and execute, but because it is not in the // message loop, it will exit after an instance of the form is created.  
+            System.Windows.Forms.Application.Run(MyForm);
+        }
     }
 }
