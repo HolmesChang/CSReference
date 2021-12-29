86fea049958066206c774be15fb6a9e3d2a9bb0f
HolmesChang <cct19840608@gmail.com>
2021-12-29 18:01:29 +0800

    Writing: Project - GUI_WinForm_SelfDevelopmentComponent_Register: Forward Struction Of GUI Component Using Result Of Parsing

--- GUI_WinForm_SelfDevelopmentComponent_Register/GUI_WinForm_SelfDevelopmentComponent_Register/MainClass.cs
+++ GUI_WinForm_SelfDevelopmentComponent_Register/GUI_WinForm_SelfDevelopmentComponent_Register/MainClass.cs
@@ -1,22 +1,65 @@
-﻿namespace GUI_WinForm_SelfDevelopmentComponent_Register
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GUI_WinForm_SelfDevelopmentComponent_Register
 {
     class MainClass : System.Windows.Forms.Form
     {
+        private const string fpath = @"D:\Publication\CS Reference\GUI_WinForm_SelfDevelopmentComponent_Register\PA1013A.csv";
+        private Register_DDI Register_DDI_PA1013 = new Register_DDI(fpath);
         public MainClass ()
         {
             // Setting Of This
-            this.Width = 600;
-            this.Height = 300;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.Text = "PA1013";
+            this.Resize += MainClass_Resize;
 
-            const string fpath = @"D:\CS Reference\GUI_WinForm_SelfDevelopmentComponent_Register\PA1013A.csv";
+            // Declaration AND Setting Of Member GUI Component Of This
+            this.Register_DDI_PA1013.Location = new System.Drawing.Point(15, 15);
+            this.Register_DDI_PA1013.Size = new Size(this.ClientSize.Width-30, this.ClientSize.Height-30);
+            this.Register_DDI_PA1013.Anchor = System.Windows.Forms.AnchorStyles.None;
+            foreach (var Page in this.Register_DDI_PA1013.ListTabPage)
+            {
+                Page.Size = new Size(Page.Parent.Size.Width, Page.Parent.Size.Height);
+                Page.ClientSize = new Size(Page.Parent.Size.Width, Page.Parent.Size.Height);
+            }
+            foreach (var TLP in this.Register_DDI_PA1013.ListTLP)
+            {
+                //TLP.Size = new Size(TLP.Parent.Size.Width-30, TLP.Parent.Size.Height-30);
+                //TLP.Width = TLP.Parent.Size.Width - 30;
+                TLP.MinimumSize = new Size(TLP.Parent.Size.Width - 30, 0);
+                TLP.AutoSize = true;
+                TLP.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            }
+            this.Controls.Add(this.Register_DDI_PA1013);
+        }
 
-            // Declaration AND Definition Of Member GUI Component
-            Register_DDI Register_DDI_PA1013 = new Register_DDI(fpath);
-            Register_DDI_PA1013.Width = 580;
-            Register_DDI_PA1013.Height = 280;
-            Register_DDI_PA1013.Anchor = System.Windows.Forms.AnchorStyles.None;
-
-            this.Controls.Add(Register_DDI_PA1013);
+        // ================================================== //
+        //  WinForm Responsive Design
+        // ================================================== //
+        private void MainClass_Resize(object sender, System.EventArgs e)
+        {
+            Size tmp = this.ClientSize;
+            this.Register_DDI_PA1013.Location = new System.Drawing.Point(15, 15);           // ReAssigning Location Is Necessary
+            this.Register_DDI_PA1013.Size = new Size(tmp.Width - 30, tmp.Height - 30);
+            //foreach (var Page in this.Register_DDI_PA1013.ListTabPage)
+            //{
+            //    Page.Size = new Size(Page.Parent.Size.Width, Page.Parent.Size.Height);
+            //    Page.ClientSize = new Size(Page.Parent.Size.Width, Page.Parent.Size.Height);
+            //}
+            //foreach (var TLP in this.Register_DDI_PA1013.ListTLP)
+            //{
+            //    //TLP.Size = new Size(TLP.Parent.Size.Width-30, TLP.Parent.Size.Height-30);
+            //    //TLP.Width = TLP.Parent.Size.Width - 30;
+            //    TLP.MinimumSize = new Size(TLP.Parent.Size.Width - 30, 0);
+            //    TLP.AutoSize = true;
+            //    TLP.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            //}
         }
 
         [System.STAThread]
--- GUI_WinForm_SelfDevelopmentComponent_Register/GUI_WinForm_SelfDevelopmentComponent_Register/Register_DDI.cs
+++ GUI_WinForm_SelfDevelopmentComponent_Register/GUI_WinForm_SelfDevelopmentComponent_Register/Register_DDI.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace GUI_WinForm_SelfDevelopmentComponent_Register
 {
     class Register_DDI : System.Windows.Forms.TabControl
     {
-        private System.String [] ListRegTable = {"B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BA"};
+        private Int32 RegParamBitWidth = 8;
 
-        private System.Windows.Forms.TabPage Page_B0h;
-        private System.Windows.Forms.TabPage Page_B1h;
-        private System.Windows.Forms.TabPage Page_B2h;
+        private System.String [] ListRegTable = {"B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BA",
+                                                 "BB", "BC", "BD", "BE", "BF", "C0", "C6", "C7", "C8", "CC", "E0", "E9",
+                                                 "EA"};
+
+        public List<TabPage> ListTabPage = new List<TabPage>();
+        public List<TableLayoutPanel> ListTLP = new List<TableLayoutPanel>();
+        public List<Button> ListButton = new List<Button>();
+        public List<TextBox> ListTextBox = new List<TextBox>();
 
         public Register_DDI (System.String fpath)
         {
@@ -54,9 +61,17 @@ namespace GUI_WinForm_SelfDevelopmentComponent_Register
                 {
                     if (!System.Array.Exists(ListRegTable, ele => ele == row[0].Trim()))
                     {
-                        ListRegDictElement["RegParamCnt"] = ((int)ListRegDictElement["RegParamCnt"]) + 1;
-                        reg_param_descr.Add(row.GetRange(2, row.Count-2));
-                        reg_param_default.Add(row[row.Count-1]);
+                        if (Int32.TryParse(row[1].Trim(), out _))
+                        {
+                            ListRegDictElement["RegParamCnt"] = ((int)ListRegDictElement["RegParamCnt"]) + 1;
+                            reg_param_descr.Add(row.GetRange(2, row.Count - 2));
+                            reg_param_default.Add(row[row.Count - 1]);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                            
                     }
                     else
                     {
@@ -102,20 +117,162 @@ namespace GUI_WinForm_SelfDevelopmentComponent_Register
                 }
             }
             // Debugging
+            // Parsing Register File
 
-            this.Page_B0h = new System.Windows.Forms.TabPage();
-            this.Page_B0h.Name = "B0h";
-            this.Page_B0h.Text = "B0h";     // Text Showing On Tab
+            // Forward Struction Of GUI Component Using Result Of Parsing Register File
+            foreach (var RegDict in ListRegDict)
+            {
+                TabPage pageTEMP = new TabPage();
+                pageTEMP.Name = ((String) RegDict["RegAddr"]).Trim() + "h";
+                pageTEMP.Text = ((String) RegDict["RegAddr"]).Trim() + "h";
+                //pageTEMP.BorderStyle = BorderStyle.FixedSingle;
+                pageTEMP.AutoScroll = true;
 
-            this.Page_B1h = new System.Windows.Forms.TabPage();
+                // Forward Struction Of TableLayoutPanel
+                System.Windows.Forms.TableLayoutPanel tlpTEMP = new TableLayoutPanel();
+                tlpTEMP.Name = "Tlp_" + (String)RegDict["RegAddr"] + "h";
+                tlpTEMP.RowCount = ((Int32)RegDict["RegParamCnt"])*2 + 1 ;
+                for (Int32 i=0; i<tlpTEMP.RowCount; i++)
+                {
+                    //tlpTEMP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1.0F/tlpTEMP.RowCount));
+                    tlpTEMP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30));
+                }
+                tlpTEMP.ColumnCount = RegParamBitWidth;
+                for (Int32 i=0; i<tlpTEMP.ColumnCount; i++)
+                {
+                    tlpTEMP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.0F/tlpTEMP.ColumnCount));
+                }
+                //tlpTEMP.BorderStyle = BorderStyle.Fixed3D;
+                //tlpTEMP.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+                tlpTEMP.Margin = new Padding(15);
+                ListTLP.Add(tlpTEMP);
+                pageTEMP.Controls.Add(tlpTEMP);
 
+                // Forward Struction Of RD/WR Button
+                System.Windows.Forms.Button btnRD = new Button();
+                btnRD.Name = "BtnRD_" + (String)RegDict["RegAddr"] + "h";
+                btnRD.Text = "RD";
+                btnRD.Dock = DockStyle.Fill;
+                btnRD.Font = new System.Drawing.Font("PMingLiU", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+                btnRD.Click += BtnRDWR_LeftClicking;
+                tlpTEMP.Controls.Add(btnRD, 0, 0);
+                tlpTEMP.SetColumnSpan(btnRD, 1);
+                ListButton.Add(btnRD);
 
-            this.Page_B2h = new System.Windows.Forms.TabPage();
+                Button btnWR = new Button();
+                btnWR.Name = "BtnWR_" + (String)RegDict["RegAddr"] + "h";
+                btnWR.Text = "WR";
+                btnWR.Dock = DockStyle.Fill;
+                btnWR.Font = new System.Drawing.Font("PMingLiU", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+                btnWR.Click += BtnRDWR_LeftClicking;
+                tlpTEMP.Controls.Add(btnWR, 1, 0);          // 2nd Parameter = Column. 3rd Parameter = Row.
+                tlpTEMP.SetColumnSpan(btnWR, 1);
+                ListButton.Add(btnWR);
+
+                Int32 row, col;
+                row = 1;
+                col = 0;
+                foreach (var ListRegParamDescr in ((List<List<String>>)RegDict["RegParamDescr"]))
+                {
+                    col = 0;
+                    foreach (var Descr in ListRegParamDescr)
+                    {
+                        if (col == 8)
+                            break;
+
+                        Label lblTEMP = new Label();
+                        lblTEMP.Text = Descr;
+                        lblTEMP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                        lblTEMP.Dock = DockStyle.Fill;
+                        lblTEMP.ForeColor = Color.FromArgb(0x000000FF);
+                        tlpTEMP.Controls.Add(lblTEMP, col, row);
+
+                        col++;
+                    }
+                    row += 2;
+                }
+
+                row = 2;
+                foreach (var RegParamDefault in ((List<String>)RegDict["RegParamDefault"]))
+                {
+                    for(Int32 j=0; j<RegParamBitWidth; j++)
+                    {
+                        TextBox tbTEMP = new TextBox();
+                        tbTEMP.Name = "TextBox_" + (String)RegDict["RegAddr"] + "h_{row}_{j}";
+                        tbTEMP.TextAlign = HorizontalAlignment.Center;
+                        tbTEMP.Dock = DockStyle.Fill;
+                        tbTEMP.Font = new System.Drawing.Font("PMingLiU", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+                        if (RegParamDefault.Trim() != "-")
+                            //if (Int32.TryParse(RegParamDefault.Trim().Substring(2, 2), out _))
+                            if (Uri.IsHexDigit(RegParamDefault.Trim()[2]) && Uri.IsHexDigit(RegParamDefault.Trim()[3]))
+                                tbTEMP.Text = ((Int32.Parse(RegParamDefault.Trim().Substring(2, 2), System.Globalization.NumberStyles.HexNumber) >> (7-j)) & 0x1).ToString();
+                        else
+                            tbTEMP.Text = "";
+                        tbTEMP.KeyPress += TB_KeyPress;
+                        tbTEMP.DoubleClick += TB_DoubleClick;
+                        ListTextBox.Add(tbTEMP);
+                        tlpTEMP.Controls.Add(tbTEMP, j, row);
+                    }
+                    row += 2;
+                }
+
+                ListTabPage.Add(pageTEMP);
+            }
 
             // Adding TabPage Into TabControl
-            this.Controls.Add(this.Page_B0h);
-            this.Controls.Add(this.Page_B1h);
-            this.Controls.Add(this.Page_B2h);
+            foreach (var Page in ListTabPage)
+            {
+                this.Controls.Add(Page);
+            }
+
+            // Only For Debugging
+            this.Selecting += new TabControlCancelEventHandler(TabControl_Selecting);
+        }
+
+        // ================================================== //
+        //
+        // ================================================== //
+        void TabControl_Selecting (object sender, TabControlCancelEventArgs e)
+        {
+            TabPage current = (sender as TabControl).SelectedTab;
+
+            Console.WriteLine(current.Name);
+            // Validate the current page. To cancel the select, use:
+            //e.Cancel = true;
+        }
+
+        // ================================================== //
+        //
+        // ================================================== //
+        void BtnRDWR_LeftClicking (object sender, EventArgs e)
+        {
+            Console.WriteLine(((Button) sender).Name);
+        }
+    
+        void TB_KeyPress (object sender, EventArgs e)
+        {
+            if (((KeyPressEventArgs)e).KeyChar == ((char)Keys.D0))
+            {
+                ((TextBox)sender).Text = String.Empty;
+                //((TextBox)sender).Text = "0";
+            }
+
+            else if (((KeyPressEventArgs)e).KeyChar == ((char)Keys.D1))
+            {
+                ((TextBox)sender).Text = String.Empty;
+                //((TextBox)sender).Text = "1";
+            }
+            else
+                ((KeyPressEventArgs)e).Handled = true;
+        }
+
+        void TB_DoubleClick (object sender, EventArgs e)
+        {
+            UInt32 tmp = Convert.ToUInt32(((TextBox)sender).Text);
+            if (tmp == 0)
+                ((TextBox)sender).Text = "1";
+            else
+                ((TextBox)sender).Text = "0";
         }
     }
 }
