1d7749654605fad2a0d9b6bb8251da244b150e9c
HolmesChang <cct19840608@gmail.com>
2022-01-01 02:33:57 +0800

    Adding: Adding Project - IO_File_JSON

---
+++ IO_File_JSON/IO_File_JSON/MainClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace IO_File_JSON
+{
+    class MainClass
+    {
+        public static void Main (String [] args)
+        {
+            Console.WriteLine("Hello I/O File JSON");
+
+            //string jsonTEMP = @"{""Name"":""HolmesChang"",""Age"":""35""}";
+
+            String fpath = @"UserConfig.json";
+            String jsonTEMP = System.IO.File.ReadAllText(fpath);
+
+            var dictTEMP = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonTEMP);
+
+            foreach (KeyValuePair<String, String> Entry in dictTEMP)
+            {
+                Console.WriteLine("{0} => {1}", Entry.Key, Entry.Value);
+            }
+            
+
+            Console.WriteLine("Please Enter Any Key To Exit");
+            Console.ReadKey();
+        }
+    }
+}
